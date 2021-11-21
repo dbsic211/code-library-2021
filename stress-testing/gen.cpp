ff3e3905226a6f1471f633a76b823a859ee7f131
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2021-11-21 17:39:03 +0800

    Update gen.cpp

--- stress-testing/gen.cpp
+++ stress-testing/gen.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 mt19937_64 rng((int)std::chrono::steady_clock::now().time_since_epoch().count());
 long long rnd(long long x, long long y) {
-    int u = uniform_int_distribution<int>(x, y)(rng);
+    long long u = uniform_int_distribution<long long>(x, y)(rng);
     return u;
 }
 int main() {
