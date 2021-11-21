341d17cd437f367043c37ae8bd047deca4929f80
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2021-11-21 16:39:20 +0800

    Create gen.cpp

---
+++ stress-testing/gen.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+using namespace std;
+mt19937_64 rng((int)std::chrono::steady_clock::now().time_since_epoch().count());
+long long rnd(long long x, long long y) {
+    int u = uniform_int_distribution<int>(x, y)(rng);
+    return u;
+}
+int main() {
+    //Write generator here
+    
+    
+}
