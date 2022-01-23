048fda6a3b509bf517bf121b8394b252d6709919
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-01-23 08:55:54 +0800

    Update fraction.cpp

--- miscellaneous/fraction.cpp
+++ miscellaneous/fraction.cpp
@@ -1,15 +1,19 @@
 struct Fraction {
-    int p, q;
-    Fraction operator+(Fraction other) {
-        Fraction sum;
-        sum.q = lcm(q, other.q);
-        sum.p = p * sum.q / q + other.p * sum.q / other.q;
-        return sum;
-    }
-    Fraction operator*(Fraction other) {
-        Fraction product;
-        product.p = p * other.p;
-        product.q = q * other.q;
-        return product;
-    }
+  int p, q;
+  Fraction operator+(Fraction other) {
+    Fraction sum;
+    sum.q = lcm(q, other.q);
+    sum.p = p * sum.q / q + other.p * sum.q / other.q;
+    int g = gcd(sum.p, sum.q);
+    sum.p /= g, sum.q /= g;
+    return sum;
+  }
+  Fraction operator*(Fraction other) {
+    Fraction product;
+    product.p = p * other.p;
+    product.q = q * other.q;
+    int g = gcd(product.p, product.q);
+    product.p /= g, product.q /= g;
+    return product;
+  }
 };
