d9ada67e4750162642a40dedb17b5cb6c7253b92
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-01-27 18:47:25 +0800

    Create reg-template-offline-ide.cpp

---
+++ miscellaneous/reg-template-offline-ide.cpp
@@ -0,0 +1,93 @@
+#pragma GCC optimize("Ofast")
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <codecvt>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+mt19937_64 rng((int)std::chrono::steady_clock::now().time_since_epoch().count());
+const int MAXN = 3e5 + 10;
+const int MOD = 1e9 + 7;
+#define int long long
+int rnd(int x, int y) {
+  int u = uniform_int_distribution<int>(x, y)(rng); return u;
+}
+void solve(int tc) {
+    
+}
+int32_t main(){
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t = 1; cin >> t;
+  for(int i=1; i<=t; i++) solve(i);
+}
