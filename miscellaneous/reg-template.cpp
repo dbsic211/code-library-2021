f572bab20294a3aa33e60bb817c8c4af1cd46c8c
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-01-27 18:42:51 +0800

    Update reg-template.cpp

--- miscellaneous/reg-template.cpp
+++ miscellaneous/reg-template.cpp
@@ -7,14 +7,14 @@ const int MOD = 1e9 + 7;
 #define int long long
 
 int rnd(int x, int y) { // random number generator
-    int u= uniform_int_distribution<int>(x, y)(rng); return u;
+  int u= uniform_int_distribution<int>(x, y)(rng); return u;
 }
 
 void solve(int tc) {
     
 }
 int32_t main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int t = 1; cin >> t;
-    for(int i=1; i<=t; i++) solve(i);
+  ios::sync_with_stdio(0); cin.tie(0);
+  int t = 1; cin >> t;
+  for(int i=1; i<=t; i++) solve(i);
 }
