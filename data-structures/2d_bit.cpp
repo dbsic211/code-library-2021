a39d40ab15a1f43865be3db97d4723acf531871b
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-02-02 19:35:25 +0800

    Create 2d_bit.cpp

---
+++ data-structures/2d_bit.cpp
@@ -0,0 +1,12 @@
+OST bit[MAXN]; // 2D bit in binary matrices
+void insert(int x, int y) {
+  for(;x<MAXN;x+=x&-x) bit[x].insert({y, x});
+}
+void erase(int x, int y) {
+  for(;x<MAXN;x+=x&-x) bit[x].erase({y, x});
+}
+int query(int x, int y) {
+  int sum = 0;
+  for(;x;x-=x&-x) sum += bit[x].order_of_key({y+1, 0});
+  return sum;
+}
