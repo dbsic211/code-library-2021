b974a1b541dec19d22f60671f64d6026aa875f54
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-02-06 01:29:03 +0800

    Update gfg_segment_intersection.cpp

--- geometry/gfg_segment_intersection.cpp
+++ geometry/gfg_segment_intersection.cpp
@@ -34,22 +34,22 @@ bool doIntersect(Point p1, Point q1, Point p2, Point q2) {
     if (o4 == 0 && onSegment(p2, q1, q2)) return true;
     return false;
 }
-Point find_intersection(Point p1, Point q1, Point p2, Point q2) {
+DoublePoint find_intersection(Point p1, Point q1, Point p2, Point q2) {
     if(!doIntersect(p1, q1, p2, q2)) return {-101, -101};
     if(p1.x == q1.x) {
         double m = (q2.y - p2.y) * 1.0 / (q2.x - p2.x);
         double c = q2.y - q2.x * m;
-        return {p1.x, m * p1.x + c};
+        return {p1.x * 1.0, m * p1.x + c};
     }
     else if(p2.x == q2.x) {
         double m = (q1.y - p1.y) * 1.0 / (q1.x - p1.x);
         double c = q1.y - q1.x * m;
-        return {p2.x, m * p2.x + c};
+        return {p2.x * 1.0, m * p2.x + c};
     }
     else {
         double m1 = (q1.y - p1.y) * 1.0 / (q1.x - p1.x);
         double c1 = q1.y - q1.x * m1;
-        double m2 = (q2.y - p2.y) * 1.0 / (q2.x - p2.x);
+        double m2 = (q2.y - p2.y) * 1.0 / (q2.x - p2.x); 
         double c2 = q2.y - q2.x * m2;
         double finx = (c2 - c1) * 1.0 / (m1 - m2);
         double finy = finx * m1 + c1;
