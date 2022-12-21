e1af7d464a94eecd1cf84c4d9e8c77939c897f11
dbsic211 <94776938+dbsic211@users.noreply.github.com>
2022-12-21 17:27:02 +0800

    Update kmp.cpp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -1,11 +1,6 @@
 vector<int> prefix_function(string t) {
-  int n = t.size();
-  vector<int> lps(n);
-  lps[0] = 0;
+  int n = t.size(); vector<int> lps(n, 0);
   for(int i=1; i<n; i++) {
-    int j = lps[i-1];
-    while(j > 0 && t[i] != t[j]) j = lps[j-1];
-    lps[i] = (t[i] == t[j] ? j+1 : 0);
-  }
-  return lps;
+    int j = lps[i-1]; while(j > 0 && t[i] != t[j]) j = lps[j-1];
+    lps[i] = (t[i] == t[j] ? j+1 : 0); } return lps;
 }
